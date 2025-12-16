42673223930ea34d40c3e91df8d1f911aadbbad2
Cameron Brown <jupiter49777@gmail.com>
2025-12-16 02:36:08 +0200

    feat: Add hoverEvents to handle changing grid div colors

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function render() {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-cell');
+    div.addEventListener('mouseenter', () => hoverEvents(div));
     divContainer.appendChild(div);
   }
+}
+
+function hoverEvents(div) {
+  div.style.backgroundColor = 'black';
 }
 
 render();
