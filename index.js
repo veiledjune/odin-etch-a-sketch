a57c3f444063564074b6a6aaaace8e2229e0440c
Cameron Brown <jupiter49777@gmail.com>
2025-12-16 03:09:04 +0200

    feat: Add updateGrid function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-function render() {
+function render(value = 16) {
   const divContainer = document.querySelector('.etch-a-sketch');
-  for (let i = 0; i < 256; i++) {
+  divContainer.textContent = '';
+  for (let i = 0; i < value * value; i++) {
     const div = document.createElement('div');
     div.classList.add('grid-cell');
+    div.style.setProperty('width', `calc(100% / ${value})`);
+    div.style.setProperty('height', `calc(100% / ${value})`);
     div.addEventListener('mouseenter', () => hoverEvents(div));
     divContainer.appendChild(div);
   }
@@ -12,4 +15,14 @@ function hoverEvents(div) {
   div.style.backgroundColor = 'black';
 }
 
+function updateGrid() {
+  const updateGridButton = document.querySelector('.change-grid-button');
+  updateGridButton.addEventListener('click', () => {
+    const userInput = Number(prompt('Update a grid size of up to 100'));
+    if (userInput && typeof userInput === 'number' && userInput <= 100)
+      render(userInput);
+  });
+}
+
+updateGrid();
 render();
